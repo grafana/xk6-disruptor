a0dfcd44e8689f8343234ee56c7d0250dbac19ed
Pablo Chacin <pablochacin@gmail.com>
2022-09-06 17:34:51 +0200

    Add option for preloading images in cluster
    Signed-off-by: Pablo Chacin <pablochacin@gmail.com>

--- pkg/testutils/cluster/cluster.go
+++ pkg/testutils/cluster/cluster.go
@@ -11,12 +11,16 @@ package cluster
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 	"time"
 
 	kind "sigs.k8s.io/kind/pkg/cluster"
+	"sigs.k8s.io/kind/pkg/cluster/nodes"
+	"sigs.k8s.io/kind/pkg/cluster/nodeutils"
 )
 
 // NodePort defines the mapping of a node port to host port
@@ -31,6 +35,9 @@ type NodePort struct {
 type ClusterOptions struct {
 	// cluster configuration to use. Overrides other options (NodePorts, Workers)
 	Config string
+	// List of images to pre-load on each node.
+	// The images must be available locally (e.g. with docker pull <image>)
+	Images []string
 	// maximum time to wait for cluster creation
 	Wait time.Duration
 	// node ports to expose
@@ -81,9 +88,42 @@ func buildClusterConfig(options ClusterOptions) (string, error) {
 	return config.String(), nil
 }
 
+// loadImages loads the images in the list to all cluster nodes' local repositories
+// TODO: check all images are available locally before creating the cluster
+// TODO: add option for attempting to pull images before loading them
+func loadImages(images []string, nodes []nodes.Node) error {
+	imagesTar, err := ioutil.TempFile(os.TempDir(), "image*.tar")
+	if err != nil {
+		return err
+	}
+	defer os.Remove(imagesTar.Name())
+
+	// save the images to a tar
+	saveCmd := append([]string{"save", "-o", imagesTar.Name()}, images...)
+	output, err := exec.Command("docker", saveCmd...).CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("error saving images: %s", string(output))
+	}
+
+	// load the image on each node of the cluster
+	for _, n := range nodes {
+		image, err := os.Open(imagesTar.Name())
+		if err != nil {
+			return err
+		}
+		err = nodeutils.LoadImageArchive(n, image)
+		image.Close()
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
 // CreateCluster creates a test cluster with the given name
 func CreateCluster(name string, options ClusterOptions) (*TestCluster, error) {
-
 	provider := kind.NewProvider()
 
 	config, err := buildClusterConfig(options)
@@ -107,6 +147,18 @@ func CreateCluster(name string, options ClusterOptions) (*TestCluster, error) {
 
 	if err != nil {
 		return nil, err
+	}
+
+	// pre-load images
+	if len(options.Images) > 0 {
+		nodes, err := provider.ListInternalNodes(name)
+		if err != nil {
+			return nil, err
+		}
+		err = loadImages(options.Images, nodes)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	configPath := filepath.Join(os.TempDir(), name)
--- pkg/testutils/cluster/cluster_e2e_test.go
+++ pkg/testutils/cluster/cluster_e2e_test.go
@@ -4,8 +4,15 @@
 package cluster
 
 import (
+	"context"
+	"os/exec"
 	"testing"
 	"time"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 func Test_DefaultConfig(t *testing.T) {
@@ -24,4 +31,95 @@ func Test_DefaultConfig(t *testing.T) {
 
 	// delete cluster
 	c.Delete()
+}
+
+func getKubernetesClient(kubeconfig string) (kubernetes.Interface, error) {
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
+
+// buildBusyboxPod returns a pod specification for running Busybox from a local image
+func buildBusyboxPod() *corev1.Pod {
+	return &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "busybox",
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name:            "busybox",
+					Image:           "busybox",
+					ImagePullPolicy: corev1.PullNever,
+				},
+			},
+		},
+	}
+}
+
+func Test_PreloadImages(t *testing.T) {
+
+	// ensure image is available locally
+	output, err := exec.Command("docker", "pull", "busybox").CombinedOutput()
+	if err != nil {
+		t.Errorf("error pulling image: %s", string(output))
+		return
+	}
+
+	// create cluster with preloaded images
+	c, err := CreateCluster(
+		"e2e-cluster-with-images",
+		ClusterOptions{
+			Wait:   time.Second * 60,
+			Images: []string{"busybox"},
+		},
+	)
+	if err != nil {
+		t.Errorf("failed to create cluster: %v", err)
+		return
+	}
+
+	defer c.Delete()
+
+	kubeconfig, err := c.Kubeconfig()
+	if err != nil {
+		t.Errorf("failed to retrieve kubeconfig: %v", err)
+		return
+	}
+
+	k8s, err := getKubernetesClient(kubeconfig)
+	if err != nil {
+		t.Errorf("error creating kubernetes client: %v", err)
+		return
+	}
+
+	pod := buildBusyboxPod()
+	_, err = k8s.CoreV1().Pods("default").Create(context.TODO(), pod, metav1.CreateOptions{})
+	if err != nil {
+		t.Errorf("failed to create pod: %v", err)
+		return
+	}
+	// FIXME: using hardcoded waits is flaky
+	time.Sleep(time.Second * 5)
+
+	pod, err = k8s.CoreV1().Pods("default").Get(context.TODO(), "busybox", metav1.GetOptions{})
+	if err != nil {
+		t.Errorf("failed to get pod: %v", err)
+		return
+	}
+
+	waiting := pod.Status.ContainerStatuses[0].State.Waiting
+	if waiting != nil && (waiting.Reason == "ErrImageNeverPull") {
+		t.Errorf("pod is waiting for image")
+		return
+	}
+
 }
