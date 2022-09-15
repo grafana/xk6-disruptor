78e5631a564d3cfd2ee29d4648be01827d470732
Pablo Chacin <pablochacin@gmail.com>
2022-09-15 17:34:01 +0200

    Implement fake kubernetes for testing
    Signed-off-by: Pablo Chacin <pablochacin@gmail.com>

---
+++ pkg/kubernetes/fake.go
@@ -0,0 +1,61 @@
+// Package fake offers a fake implementation of the Kubernetes interface
+package kubernetes
+
+import (
+	"fmt"
+
+	"github.com/grafana/xk6-disruptor/pkg/kubernetes/helpers"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes/fake"
+)
+
+// FakeKubernetes is a fake implementation of the Kubernetes interface
+type FakeKubernetes struct {
+	*fake.Clientset
+	executor *helpers.FakePodCommandExecutor
+}
+
+// NewFakeKubernetes returns a new fake implementation of Kubernetes from fake Clientset
+func NewFakeKubernetes(clientset *fake.Clientset) (*FakeKubernetes, error) {
+	return &FakeKubernetes{
+		Clientset: clientset,
+		executor:  helpers.NewFakePodCommandExecutor(),
+	}, nil
+}
+
+// Helpers return a instance of FakeHelper
+func (f *FakeKubernetes) Helpers() helpers.Helpers {
+	return helpers.NewFakeHelper(
+		f.Clientset,
+		"default",
+		f.executor,
+	)
+}
+
+// NamespacedHelpers return a instance of FakeHelper for a given namespace
+func (f *FakeKubernetes) NamespacedHelpers(namespace string) helpers.Helpers {
+	return helpers.NewFakeHelper(
+		f.Clientset,
+		namespace,
+		f.executor,
+	)
+}
+
+// GetFakeProcessExecutor returns the FakeProcessExecutor used by the helpers to mock
+// the execution of commands in a Pod
+func (f *FakeKubernetes) GetFakeProcessExecutor() *helpers.FakePodCommandExecutor {
+	return f.executor
+}
+
+func (f *FakeKubernetes) Create(manifest string) error {
+	return fmt.Errorf("operation not supported")
+}
+
+func (f *FakeKubernetes) Get(kind string, name string, namespace string, obj runtime.Object) error {
+	return fmt.Errorf("operation not supported")
+}
+
+func (f *FakeKubernetes) Delete(kind string, name string, namespace string) error {
+	return fmt.Errorf("operation not supported")
+}
---
+++ pkg/kubernetes/helpers/fake.go
@@ -0,0 +1,84 @@
+package helpers
+
+import (
+	"context"
+	"sync"
+
+	"k8s.io/client-go/kubernetes"
+)
+
+// Command records the execution of a command in a Pod
+type Command struct {
+	Pod       string
+	Container string
+	Command   []string
+	Stdin     []byte
+}
+
+// PodCommandExecutor mocks the execution of a command in a pod
+// recording the command history and returning a predefined stdout, stderr, and error
+type FakePodCommandExecutor struct {
+	mutex   sync.Mutex
+	history []Command
+	stdout  []byte
+	stderr  []byte
+	err     error
+}
+
+// Exec records the execution of a command and returns the pre-defined
+func (f *FakePodCommandExecutor) Exec(pod string, container string, cmd []string, stdin []byte) ([]byte, []byte, error) {
+	f.mutex.Lock()
+	f.history = append(f.history, Command{
+		Pod:       pod,
+		Container: container,
+		Command:   cmd,
+		Stdin:     stdin,
+	})
+	f.mutex.Unlock()
+
+	return f.stdout, f.stderr, f.err
+}
+
+// SetResult sets the results to be returned for each invocation to the FakePodCommandExecutor
+func (f *FakePodCommandExecutor) SetResult(stdout []byte, stderr []byte, err error) {
+	f.stdout = stdout
+	f.stderr = stderr
+	f.err = err
+}
+
+// GetHistory returns the history of commands executed by the FakePodCommandExecutor
+func (f *FakePodCommandExecutor) GetHistory() []Command {
+	return f.history
+}
+
+// NewFakePodCommandExecutor creates a new instance of FakePodCommandExecutor
+// with default attributes
+func NewFakePodCommandExecutor() *FakePodCommandExecutor {
+	return &FakePodCommandExecutor{}
+}
+
+// fakeHelper is an fake instance of a Helpers. It can delegate to the actual
+// helpers the execution of actions or override them when needed
+type fakeHelper struct {
+	*helpers
+	executor *FakePodCommandExecutor
+}
+
+// NewHelpers creates a set of helpers on the default namespace
+func NewFakeHelper(client kubernetes.Interface, namespace string, executor *FakePodCommandExecutor) Helpers {
+	helpers := &helpers{
+		client:    client,
+		namespace: namespace,
+		ctx:       context.TODO(),
+	}
+
+	return &fakeHelper{
+		helpers,
+		executor,
+	}
+}
+
+// Fakes the execution of a command in a pod
+func (f *fakeHelper) Exec(pod string, container string, command []string, stdin []byte) ([]byte, []byte, error) {
+	return f.executor.Exec(pod, container, command, stdin)
+}
