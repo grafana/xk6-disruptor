3856e7ad844ad4a776aab161966978214f4ede54
Pablo Chacin <pablochacin@gmail.com>
2022-10-25 20:02:15 +0200

    Change case of exported struct fields
    Signed-off-by: Pablo Chacin <pablochacin@gmail.com>

--- examples/httpbin/disrupt-pod.js
+++ examples/httpbin/disrupt-pod.js
@@ -59,9 +59,9 @@ export function disrupt(data) {
 
   // delay traffic from one random replica of the deployment
   const fault = {
-      average_delay: 50,
-      error_code: 500,
-      error_rate: 0.1
+      averageDelay: 50,
+      errorCode: 500,
+      errorRate: 0.1
   }
   podDisruptor.injectHTTPFaults(fault, 30)
 }
