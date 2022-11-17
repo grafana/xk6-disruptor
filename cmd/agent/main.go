3251b5568349ab4524b1a31b53d2e455b461dc3f
Pablo Chacin <pablochacin@gmail.com>
2022-11-17 17:20:44 +0100

    Suppress usage message on errors invoking agent
    Signed-off-by: Pablo Chacin <pablochacin@gmail.com>

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -9,11 +9,14 @@ import (
 )
 
 func main() {
+
 	rootCmd := &cobra.Command{
 		Use:   "xk6-disruptor-agent",
 		Short: "Inject disruptions in a system",
 		Long: "A command for injecting disruptions in a target system.\n" +
 			"It can run as stand-alone process or in a container",
+		SilenceUsage:  true,
+		SilenceErrors: true,
 	}
 
 	rootCmd.AddCommand(commands.BuildHTTPCmd())
