f46006acfa7aa712f982521e5e2606f52e064f2a
Pablo Chacin <pablochacin@gmail.com>
2022-11-18 14:11:59 +0100

    Refactor e2 test to allow tests sharing cluster
    Signed-off-by: Pablo Chacin <pablochacin@gmail.com>

--- e2e/api/pod_e2e_test.go
+++ e2e/api/pod_e2e_test.go
@@ -16,13 +16,15 @@ import (
 	"github.com/grafana/xk6-disruptor/pkg/testutils/e2e/fixtures"
 )
 
+
 func Test_PodDisruptor(t *testing.T) {
+	t.Parallel()
+
 	cluster, err := fixtures.BuildCluster("e2e-pod-disruptor")
 	if err != nil {
 		t.Errorf("failed to create cluster config: %v", err)
 		return
 	}
-	defer cluster.Delete()
 
 	k8s, err := kubernetes.NewFromKubeconfig(cluster.Kubeconfig())
 	if err != nil {
@@ -30,79 +32,108 @@ func Test_PodDisruptor(t *testing.T) {
 		return
 	}
 
-	t.Run("Inject Http error 500", func(t *testing.T) {
-		ns, err := k8s.Helpers().CreateRandomNamespace("test-pods")
-		if err != nil {
-			t.Errorf("error creating test namespace: %v", err)
-			return
-		}
-		defer k8s.CoreV1().Namespaces().Delete(context.TODO(), ns, metav1.DeleteOptions{})
-
-		nodePort := cluster.AllocatePort()
-		if nodePort.HostPort == 0 {
-			t.Errorf("no nodeport available for test")
-			return
-		}
-		defer cluster.ReleasePort(nodePort)
-
-		err = fixtures.DeployApp(
-			k8s,
-			ns,
-			fixtures.BuildHttpbinPod(),
-			fixtures.BuildHttpbinService(nodePort.NodePort),
-			20*time.Second,
-		)
-		if err != nil {
-			t.Errorf("error deploying httpbin: %v", err)
-			return
-		}
-
-		// create pod disruptor
-		selector := disruptors.PodSelector{
-			Namespace: ns,
-			Select: disruptors.PodAttributes{
-				Labels: map[string]string{
-					"app": "httpbin",
-				},
-			},
-		}
-		options := disruptors.PodDisruptorOptions{}
-		disruptor, err := disruptors.NewPodDisruptor(k8s, selector, options)
-		if err != nil {
-			t.Errorf("error creating selector: %v", err)
-			return
-		}
-
-		targets, _ := disruptor.Targets()
-		if len(targets) == 0 {
-			t.Errorf("No pods matched the selector")
-			return
-		}
-
-		// apply disruption in a go-routine as it is a blocking function
-		go func() {
-			fault := disruptors.HTTPFault{
+	testCases := []struct{
+		title       string
+		fault       disruptors.HTTPFault
+		options     disruptors.HTTPDisruptionOptions
+		expectError bool
+		checkCode   int
+	} {
+		{
+			title:  "Inject Http error 500",
+			fault: 	disruptors.HTTPFault{
 				Port:      80,
 				ErrorRate: 1.0,
 				ErrorCode: 500,
-			}
-			opts := disruptors.HTTPDisruptionOptions{
+			},
+			options:  disruptors.HTTPDisruptionOptions{
 				ProxyPort: 8080,
-			}
-			err := disruptor.InjectHTTPFaults(fault, 10, opts)
-			if err != nil {
-				t.Errorf("error injecting fault: %v", err)
-			}
-		}()
+			},
+			expectError: false,
+			checkCode: 500,
+		},
+	}
 
-		err = checks.CheckService(checks.ServiceCheck{
-			Port:         nodePort.HostPort,
-			Delay:        2 * time.Second,
-			ExpectedCode: 500,
-		})
-		if err != nil {
-			t.Errorf("failed to access service: %v", err)
-			return
-		}
+	t.Cleanup(func(){
+		cluster.Delete()
 	})
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.title, func(t *testing.T) {
+			t.Parallel()
+
+			ns, err := k8s.Helpers().CreateRandomNamespace("test-pods")
+			if err != nil {
+				t.Errorf("error creating test namespace: %v", err)
+				return
+			}
+			defer k8s.CoreV1().Namespaces().Delete(context.TODO(), ns, metav1.DeleteOptions{})
+
+			nodePort := cluster.AllocatePort()
+			if nodePort.HostPort == 0 {
+				t.Errorf("no nodeport available for test")
+				return
+			}
+			defer cluster.ReleasePort(nodePort)
+
+			err = fixtures.DeployApp(
+				k8s,
+				ns,
+				fixtures.BuildHttpbinPod(),
+				fixtures.BuildHttpbinService(nodePort.NodePort),
+				30*time.Second,
+			)
+			if err != nil {
+				t.Errorf("error deploying httpbin: %v", err)
+				return
+			}
+
+			// create pod disruptor
+			selector := disruptors.PodSelector{
+				Namespace: ns,
+				Select: disruptors.PodAttributes{
+					Labels: map[string]string{
+						"app": "httpbin",
+					},
+				},
+			}
+			options := disruptors.PodDisruptorOptions{}
+			disruptor, err := disruptors.NewPodDisruptor(k8s, selector, options)
+			if err != nil {
+				t.Errorf("error creating selector: %v", err)
+				return
+			}
+
+			targets, _ := disruptor.Targets()
+			if len(targets) == 0 {
+				t.Errorf("No pods matched the selector")
+				return
+			}
+
+			// apply disruption in a go-routine as it is a blocking function
+			go func() {
+				err := disruptor.InjectHTTPFaults(tc.fault, 10, tc.options)
+				if tc.expectError && err == nil {
+					t.Errorf("should had failed")
+					return
+				}
+
+				if !tc.expectError && err != nil {
+					t.Errorf("failed: %v", err)
+					return
+				}
+			}()
+
+			err = checks.CheckService(checks.ServiceCheck{
+				Port:         nodePort.HostPort,
+				Delay:        2 * time.Second,
+				ExpectedCode: tc.checkCode,
+			})
+			if err != nil {
+				t.Errorf("failed to access service: %v", err)
+				return
+			}
+		})
+	}
 }
