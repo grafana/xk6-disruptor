dc6f5c45228618b3eb55f3082a97b01a1ec961f4
Pablo Chacin <pablochacin@gmail.com>
2022-11-20 12:21:37 +0100

    Test error handling in traffic redirector
    Signed-off-by: Pablo Chacin <pablochacin@gmail.com>

--- pkg/iptables/iptables_test.go
+++ pkg/iptables/iptables_test.go
@@ -1,6 +1,7 @@
 package iptables
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 
@@ -93,6 +94,8 @@ func Test_Commands(t *testing.T) {
 		redirect     TrafficRedirectionSpec
 		expectedCmds []string
 		expectError  bool
+		fakeError    error
+		fakeOutput   []byte
 		testFunction func(TrafficRedirector) error
 	}{
 		{
@@ -111,6 +114,8 @@ func Test_Commands(t *testing.T) {
 				"iptables -A INPUT -i eth0 -p tcp --dport 80 -m state --state ESTABLISHED -j REJECT --reject-with tcp-reset",
 			},
 			expectError: false,
+			fakeError:   nil,
+			fakeOutput:  []byte{},
 		},
 		{
 			title: "Stop active redirect",
@@ -128,6 +133,38 @@ func Test_Commands(t *testing.T) {
 				"iptables -D INPUT -i eth0 -p tcp --dport 80 -m state --state ESTABLISHED -j REJECT --reject-with tcp-reset",
 			},
 			expectError: false,
+			fakeError:   nil,
+			fakeOutput:  []byte{},
+		},
+		{
+			title: "Error invoking iptables command in Start",
+			redirect: TrafficRedirectionSpec{
+				Iface:           "eth0",
+				DestinationPort: 80,
+				RedirectPort:    8080,
+			},
+			testFunction: func(tr TrafficRedirector) error {
+				return tr.Start()
+			},
+			expectedCmds: []string{},
+			expectError:  true,
+			fakeError:    fmt.Errorf("process exited with return code 1"),
+			fakeOutput:   []byte{},
+		},
+		{
+			title: "Error invoking iptables command in Stop",
+			redirect: TrafficRedirectionSpec{
+				Iface:           "eth0",
+				DestinationPort: 80,
+				RedirectPort:    8080,
+			},
+			testFunction: func(tr TrafficRedirector) error {
+				return tr.Stop()
+			},
+			expectedCmds: []string{},
+			expectError:  true,
+			fakeError:    fmt.Errorf("process exited with return code 1"),
+			fakeOutput:   []byte{},
 		},
 	}
 
@@ -137,7 +174,7 @@ func Test_Commands(t *testing.T) {
 		t.Run(tc.title, func(t *testing.T) {
 			t.Parallel()
 
-			executor := process.NewFakeExecutor([]byte{}, nil)
+			executor := process.NewFakeExecutor(tc.fakeOutput, tc.fakeError)
 			config := TrafficRedirectorConfig{
 				Executor: executor,
 			}
@@ -151,7 +188,7 @@ func Test_Commands(t *testing.T) {
 			err = tc.testFunction(redirector)
 
 			if !tc.expectError && err != nil {
-				t.Errorf("failed with error %v", err)
+				t.Errorf("failed with error: %v", err)
 				return
 			}
 
