0b9e69b64f980fe1c585837516cc531b0f91d035
Pablo Chacin <pablochacin@gmail.com>
2022-12-23 15:47:39 +0100

    Add validations to PodDisruptor methods
    Signed-off-by: Pablo Chacin <pablochacin@gmail.com>

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -9,7 +9,87 @@ import (
 	"github.com/dop251/goja"
 	"github.com/grafana/xk6-disruptor/pkg/disruptors"
 	"github.com/grafana/xk6-disruptor/pkg/kubernetes"
+	"go.k6.io/k6/js/common"
 )
+
+// converts a goja.Value into a object. target is expected to be a pointer
+func convertValue(rt *goja.Runtime, value goja.Value, target interface{}) error {
+	// get the value pointed to by the target and check for compatibility
+	err := IsCompatible(value.Export(), reflect.ValueOf(target).Elem().Interface())
+	if err != nil {
+		return err
+	}
+
+	err = rt.ExportTo(value, target)
+	return err
+}
+
+// JsPodDisruptor implements the JS interface for PodDisruptor
+type JsPodDisruptor struct {
+	rt        *goja.Runtime
+	disruptor disruptors.PodDisruptor
+}
+
+// Targets is a proxy method. Validates parameters and delegates to the PodDisruptor method
+func (p *JsPodDisruptor) Targets() goja.Value {
+	targets, err := p.disruptor.Targets()
+	if err != nil {
+		common.Throw(p.rt, fmt.Errorf("error getting kubernetes config path: %w", err))
+	}
+
+	return p.rt.ToValue(targets)
+}
+
+// InjectHTTPFaults is a proxy method. Validates parameters and delegates to the PodDisruptor method
+func (p *JsPodDisruptor) InjectHTTPFaults(args ...goja.Value) {
+	if len(args) < 2 {
+		common.Throw(p.rt, fmt.Errorf("HTTPFault and duration are required"))
+	}
+
+	fault := disruptors.HTTPFault{}
+	err := convertValue(p.rt, args[0], &fault)
+	if err != nil {
+		common.Throw(p.rt, fmt.Errorf("invalid fault argument: %w", err))
+	}
+
+	duration := args[1].ToInteger()
+	if duration < 0 {
+		common.Throw(p.rt, fmt.Errorf("duration must be non-negative"))
+	}
+
+	opts := disruptors.HTTPDisruptionOptions{}
+	if len(args) > 2 {
+		err = convertValue(p.rt, args[2], &opts)
+		if err != nil {
+			common.Throw(p.rt, fmt.Errorf("invalid options argument: %w", err))
+		}
+	}
+
+	err = p.disruptor.InjectHTTPFaults(fault, uint(duration), opts)
+	if err != nil {
+		common.Throw(p.rt, fmt.Errorf("error injecting fault: %w", err))
+	}
+}
+
+func buildJsPodDisruptor(rt *goja.Runtime, disruptor disruptors.PodDisruptor) (*goja.Object, error) {
+	jsDisruptor := JsPodDisruptor{
+		rt:        rt,
+		disruptor: disruptor,
+	}
+
+	obj := rt.NewObject()
+	err := obj.Set("targets", jsDisruptor.Targets)
+	if err != nil {
+		return nil, err
+	}
+
+	err = obj.Set("injectHTTPFaults", jsDisruptor.InjectHTTPFaults)
+	if err != nil {
+		return nil, err
+	}
+
+	return obj, nil
+}
 
 // NewPodDisruptor creates an instance of a PodDisruptor
 func NewPodDisruptor(rt *goja.Runtime, c goja.ConstructorCall, k8s kubernetes.Kubernetes) (*goja.Object, error) {
@@ -18,11 +98,7 @@ func NewPodDisruptor(rt *goja.Runtime, c goja.ConstructorCall, k8s kubernetes.Ku
 	}
 
 	selector := disruptors.PodSelector{}
-	err := IsCompatible(c.Argument(0).Export(), selector)
-	if err != nil {
-		return nil, fmt.Errorf("invalid PodSelector: %w", err)
-	}
-	err = rt.ExportTo(c.Argument(0), &selector)
+	err := convertValue(rt, c.Argument(0), &selector)
 	if err != nil {
 		return nil, fmt.Errorf("invalid PodSelector: %w", err)
 	}
@@ -30,11 +106,7 @@ func NewPodDisruptor(rt *goja.Runtime, c goja.ConstructorCall, k8s kubernetes.Ku
 	options := disruptors.PodDisruptorOptions{}
 	// options argument is optional
 	if len(c.Arguments) > 1 {
-		err = IsCompatible(c.Argument(1).Export(), options)
-		if err != nil {
-			return nil, fmt.Errorf("invalid PodDisruptorOptions: %w", err)
-		}
-		err = rt.ExportTo(c.Argument(1), &options)
+		err = convertValue(rt, c.Argument(1), &options)
 		if err != nil {
 			return nil, fmt.Errorf("invalid PodDisruptorOptions: %w", err)
 		}
@@ -45,7 +117,12 @@ func NewPodDisruptor(rt *goja.Runtime, c goja.ConstructorCall, k8s kubernetes.Ku
 		return nil, fmt.Errorf("error creating PodDisruptor: %w", err)
 	}
 
-	return rt.ToValue(disruptor).ToObject(rt), nil
+	obj, err := buildJsPodDisruptor(rt, disruptor)
+	if err != nil {
+		return nil, fmt.Errorf("error creating PodDisruptor: %w", err)
+	}
+
+	return obj, nil
 }
 
 // NewServiceDisruptor creates an instance of a ServiceDisruptor
--- pkg/api/api_test.go
+++ pkg/api/api_test.go
@@ -151,7 +151,7 @@ func Test_PodDisruptorConstructor(t *testing.T) {
 				return
 			}
 
-			value, err := env.rt.RunString(tc.script)
+			_, err = env.rt.RunString(tc.script)
 
 			if !tc.expectError && err != nil {
 				t.Errorf("failed %v", err)
@@ -162,16 +162,144 @@ func Test_PodDisruptorConstructor(t *testing.T) {
 				t.Errorf("should had failed")
 				return
 			}
+		})
+	}
+}
 
-			// failed and it was expected, it is ok
-			if tc.expectError && err != nil {
+const setupPodDisruptor = `
+	const selector = {
+	namespace: "namespace",
+	select: {
+		labels: {
+			app: "app"
+		}
+	}
+}
+const opts = {
+	injectTimeout: 0
+}
+const d = new PodDisruptor(selector, opts)
+`
+
+func Test_JsPodDisruptor(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		description string
+		script      string
+		expectError bool
+	}{
+		{
+			description: "get targets",
+			script: `
+			d.targets()
+			`,
+			expectError: false,
+		},
+		{
+			description: "inject HTTP Fault with full arguments",
+			script: `
+			const fault = {
+				errorRate: 1.0,
+				errorCode: 500,
+				averageDelay: 100,
+				delayVariation: 10,
+				errorBody: '',
+				exclude: "",
+				port: 80
+			}
+
+			const faultOpts = {
+				proxyPort: 8080,
+				iface: "eth0"
+			}
+
+			d.injectHTTPFaults(fault, 1, faultOpts)
+			`,
+			expectError: false,
+		},
+		{
+			description: "inject HTTP Fault without options",
+			script: `
+			const fault = {
+				errorRate: 1.0,
+				errorCode: 500,
+				averageDelay: 100,
+				delayVariation: 10,
+				errorBody: '',
+				exclude: "",
+				port: 80
+			}
+
+			d.injectHTTPFaults(fault, 1)
+			`,
+			expectError: false,
+		},
+		{
+			description: "inject HTTP Fault without duration",
+			script: `
+			const fault = {
+				errorRate: 1.0,
+				errorCode: 500,
+				averageDelay: 100,
+				delayVariation: 10,
+				errorBody: '',
+				exclude: "",
+				port: 80
+			}
+
+			d.injectHTTPFaults(fault)
+			`,
+			expectError: true,
+		},
+		{
+			description: "inject HTTP Fault with malformed fault (misspelled field)",
+			script: `
+			const fault = {
+				errorRate: 1.0,
+				error: 500,       // this is should be 'errorCode'
+			}
+			
+			d.injectHTTPFaults(fault, 1)
+			`,
+			expectError: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.description, func(t *testing.T) {
+			t.Parallel()
+
+			env, err := testSetup()
+			if err != nil {
+				t.Errorf("error in test setup %v", err)
 				return
 			}
 
-			var pd disruptors.PodDisruptor
-			err = env.rt.ExportTo(value, &pd)
+			err = env.registerConstructor("PodDisruptor", func(e *testEnv, c goja.ConstructorCall) (*goja.Object, error) {
+				return NewPodDisruptor(e.rt, c, e.k8s)
+			})
 			if err != nil {
-				t.Errorf("returned valued cannot be converted to PodDisruptor: %v", err)
+				t.Errorf("error in test setup %v", err)
+				return
+			}
+
+			_, err = env.rt.RunString(setupPodDisruptor)
+			if err != nil {
+				t.Errorf("error in test setup %v", err)
+				return
+			}
+
+			_, err = env.rt.RunString(tc.script)
+
+			if !tc.expectError && err != nil {
+				t.Errorf("failed %v", err)
+				return
+			}
+
+			if tc.expectError && err == nil {
+				t.Errorf("should had failed")
 				return
 			}
 		})
