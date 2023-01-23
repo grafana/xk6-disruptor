aa1e9139271dd25dc31aa196ae1afb82e5676f61
Pablo Chacin <pablochacin@gmail.com>
2023-01-23 21:07:44 +0100

    strip symbols from binary
    Signed-off-by: Pablo Chacin <pablochacin@gmail.com>

--- build-package.sh
+++ build-package.sh
@@ -67,7 +67,7 @@ function build() {
 
    export GOARCH=$arch
    export GOOS=$os
-   export XK6_BUILD_FLAGS='-ldflags "-X github.com/grafana/xk6-disruptor/pkg/internal/consts.Version='${version}'"'
+   export XK6_BUILD_FLAGS='-ldflags "-w -s -X github.com/grafana/xk6-disruptor/pkg/internal/consts.Version='${version}'"'
    xk6 build --with $(go list -m)=. --with github.com/grafana/xk6-kubernetes --output $BUILD/$binary
   )
 }
