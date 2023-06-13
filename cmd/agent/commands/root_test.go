b4285c0448207fce52c10f6a17ddf82116872eca
Pablo Chacin <pablochacin@gmail.com>
2023-06-13 18:27:10 +0200

    Test singnal handling in root command
    Signed-off-by: Pablo Chacin <pablochacin@gmail.com>

--- cmd/agent/commands/root_test.go
+++ cmd/agent/commands/root_test.go
@@ -3,6 +3,7 @@ package commands
 import (
 	"context"
 	"errors"
+	"os"
 	"testing"
 	"time"
 
@@ -73,6 +74,68 @@ func Test_CancelContext(t *testing.T) {
 			err := rootCmd.Do(ctx)
 			if !errors.Is(err, tc.err) {
 				t.Errorf("expected %v got %v", tc.err, err)
+			}
+		})
+	}
+}
+
+func Test_Signals(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		title     string
+		args      []string
+		vars      map[string]string
+		subcmds   []*cobra.Command
+		signal    os.Signal
+		expectErr bool
+	}{
+		{
+			title: "Command is canceled with interrupt",
+			args:  []string{"xk6-disruptor", "noop", "-d", "5s"},
+			vars:  map[string]string{},
+			subcmds: []*cobra.Command{
+				BuildNoopCmd(),
+			},
+			signal:    os.Interrupt,
+			expectErr: true,
+		},
+		{
+			title: "Command is not canceled with interrupt",
+			args:  []string{"xk6-disruptor", "noop", "-d", "5s"},
+			vars:  map[string]string{},
+			subcmds: []*cobra.Command{
+				BuildNoopCmd(),
+			},
+			signal:    nil,
+			expectErr: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.title, func(t *testing.T) {
+			t.Parallel()
+			env := runtime.NewFakeRuntime(tc.args, tc.vars)
+
+			rootCmd := BuildRootCmd(env, tc.subcmds)
+
+			go func() {
+				time.Sleep(1 * time.Second)
+				if tc.signal != nil {
+					env.FakeSignal.SendSignal(tc.signal)
+				}
+			}()
+
+			err := rootCmd.Do(context.Background())
+			if tc.expectErr && err == nil {
+				t.Errorf("should had failed")
+				return
+			}
+
+			if !tc.expectErr && err != nil {
+				t.Errorf("unexpected error: %v", err)
+				return
 			}
 		})
 	}
