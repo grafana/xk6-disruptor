24aa071b09a9d1cde0dbb332b79a617445672f7f
Pablo Chacin <pablochacin@gmail.com>
2023-06-30 11:43:47 +0200

    Fix namespace tests
    Signed-off-by: Pablo Chacin <pablochacin@gmail.com>

--- pkg/testutils/e2e/kubernetes/namespace/namespace_test.go
+++ pkg/testutils/e2e/kubernetes/namespace/namespace_test.go
@@ -2,11 +2,33 @@ package namespace
 
 import (
 	"context"
+	"fmt"
+	"regexp"
 	"testing"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/rand"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/fake"
+	k8stesting "k8s.io/client-go/testing"
 )
+
+// generates name for a resource
+func generateName(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
+	//nolint:forcetypeassert
+	ret = action.(k8stesting.CreateAction).GetObject()
+	meta, ok := ret.(metav1.Object)
+	if !ok {
+		return
+	}
+
+	if meta.GetName() == "" && meta.GetGenerateName() != "" {
+		meta.SetName(meta.GetGenerateName() + rand.String(5))
+	}
+
+	return
+}
 
 func Test_CreateNamespace(t *testing.T) {
 	t.Parallel()
@@ -22,14 +44,22 @@ func Test_CreateNamespace(t *testing.T) {
 			options:     []TestNamespaceOption{},
 			expectError: false,
 			check: func(k8s kubernetes.Interface, ns string) error {
+				match, _ := regexp.MatchString(`testns-[a-z|0-9]+`, ns)
+				if !match {
+					return fmt.Errorf("expected pattern 'testns-xxxxx' got %q", ns)
+				}
 				return nil
 			},
 		},
 		{
-			title:       "random namespace",
-			options:     []TestNamespaceOption{WithPrefix("prefix-")},
+			title:       "fixed name",
+			options:     []TestNamespaceOption{WithName("testns")},
 			expectError: false,
 			check: func(k8s kubernetes.Interface, ns string) error {
+				if ns != "testns" {
+					return fmt.Errorf("expected 'testns' got %q", ns)
+				}
+
 				return nil
 			},
 		},
@@ -41,6 +71,9 @@ func Test_CreateNamespace(t *testing.T) {
 			t.Parallel()
 
 			client := fake.NewSimpleClientset()
+			// Fake client does not support generated named
+			client.PrependReactor("create", "*", generateName)
+
 			ns, err := CreateTestNamespace(context.TODO(), t, client, tc.options...)
 			if err != nil && !tc.expectError {
 				t.Errorf("unexpected error %v", err)
