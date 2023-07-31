2189696998f847f9ad5c92d6e4c16816234c7303
Roberto Santalla <roberto@santalla.io>
2023-07-31 11:56:10 +0200

    disruptors: error out if there are no targets

--- pkg/disruptors/pod.go
+++ pkg/disruptors/pod.go
@@ -3,8 +3,10 @@ package disruptors
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"reflect"
+	"strings"
 	"time"
 
 	"github.com/grafana/xk6-disruptor/pkg/kubernetes"
@@ -17,6 +19,10 @@ import (
 
 // DefaultTargetPort defines default target port if not specified in Fault
 const DefaultTargetPort = 80
+
+// ErrSelectorNoPods is returned by NewPodDisruptor when the selector passed to it does not match any pod in the
+// cluster.
+var ErrSelectorNoPods = errors.New("no pods found matching selector")
 
 // PodDisruptor defines the types of faults that can be injected in a Pod
 type PodDisruptor interface {
@@ -50,6 +56,52 @@ type PodAttributes struct {
 	Labels map[string]string
 }
 
+// NamespaceOrDefault returns the configured namespace for this selector, and the name of the default namespace if it
+// is not configured.
+func (p PodSelector) NamespaceOrDefault() string {
+	if p.Namespace != "" {
+		return p.Namespace
+	}
+
+	return metav1.NamespaceDefault
+}
+
+// String returns a human-readable explanation of the pods matched by a PodSelector.
+func (p PodSelector) String() string {
+	var str string
+
+	if len(p.Select.Labels) == 0 && len(p.Exclude.Labels) == 0 {
+		str = "all pods"
+	} else {
+		str = "pods "
+		str += p.groupLabels("including", p.Select.Labels)
+		str += p.groupLabels("excluding", p.Exclude.Labels)
+		str = strings.TrimSuffix(str, ", ")
+	}
+
+	str += fmt.Sprintf(" in ns %q", p.NamespaceOrDefault())
+
+	return str
+}
+
+// groupLabels returns a group of labels as a string, giving that group a name. The returned string has the form of:
+// `groupName(foo=bar, boo=baz), `, including the trailing space and comma.
+// An empty group of labels produces an empty string.
+func (PodSelector) groupLabels(groupName string, labels map[string]string) string {
+	if len(labels) == 0 {
+		return ""
+	}
+
+	group := groupName + "("
+	for k, v := range labels {
+		group += fmt.Sprintf("%s=%s, ", k, v)
+	}
+	group = strings.TrimSuffix(group, ", ")
+	group += "), "
+
+	return group
+}
+
 // NewPodDisruptor creates a new instance of a PodDisruptor that acts on the pods
 // that match the given PodSelector
 func NewPodDisruptor(
@@ -66,10 +118,7 @@ func NewPodDisruptor(
 	}
 
 	// ensure selector and controller use default namespace if none specified
-	namespace := selector.Namespace
-	if selector.Namespace == "" {
-		selector.Namespace = metav1.NamespaceDefault
-	}
+	namespace := selector.NamespaceOrDefault()
 	helper := k8s.PodHelper(namespace)
 
 	filter := helpers.PodFilter{
@@ -80,6 +129,10 @@ func NewPodDisruptor(
 	targets, err := helper.List(ctx, filter)
 	if err != nil {
 		return nil, err
+	}
+
+	if len(targets) == 0 {
+		return nil, fmt.Errorf("finding pods matching '%s': %w", selector, ErrSelectorNoPods)
 	}
 
 	controller := NewAgentController(
