6809fceb64caa38887b229f03bee6e82ccc20eb3
Nikos Angelopoulos <nikolaos.angelopoulos@grafana.com>
2025-07-03 15:31:23 +0200

    agent: add cobra command

--- cmd/agent/commands/networkdrop.go
+++ cmd/agent/commands/networkdrop.go
@@ -5,23 +5,22 @@ import (
 	"time"
 
 	"github.com/grafana/xk6-disruptor/pkg/agent"
-	"github.com/grafana/xk6-disruptor/pkg/agent/tcpconn"
+	"github.com/grafana/xk6-disruptor/pkg/agent/network"
 	"github.com/grafana/xk6-disruptor/pkg/iptables"
 	"github.com/grafana/xk6-disruptor/pkg/runtime"
 	"github.com/spf13/cobra"
 )
 
-// BuildTCPDropCmd returns a cobra command with the specification of the tcp-drop command.
-func BuildTCPDropCmd(env runtime.Environment, config *agent.Config) *cobra.Command {
+// BuildNetworkDropCmd builds the command for dropping network traffic on a given port.
+func BuildNetworkDropCmd(env runtime.Environment, config *agent.Config) *cobra.Command {
 	var duration time.Duration
-	filter := tcpconn.Filter{}
-	dropRate := 0.0
+	filter := network.Filter{}
 
 	cmd := &cobra.Command{
-		Use:   "tcp-drop",
-		Short: "tcp connection drop",
-		Long: "Disrupts TCP connections by terminating a certain percentage of them." +
-			" Requires either to be run as root, or the NET_ADMIN capability.",
+		Use:   "network-drop",
+		Short: "network connection drop",
+		Long: "Drops network traffic on a given port. Requires either to be run as root, or the NET_ADMIN capability." +
+			" Unlike tcp-drop, this command drops network traffic without sending any response to the client.",
 		RunE: func(cmd *cobra.Command, _ []string) error {
 			if filter.Port == 0 {
 				return fmt.Errorf("target port for fault injection is required")
@@ -34,23 +33,17 @@ func BuildTCPDropCmd(env runtime.Environment, config *agent.Config) *cobra.Comma
 
 			defer agent.Stop()
 
-			dropper := tcpconn.TCPConnectionDropper{
-				DropRate: dropRate,
-			}
-
-			disruptor := tcpconn.Disruptor{
+			disruptor := network.Disruptor{
 				Iptables: iptables.New(env.Executor()),
 				Filter:   filter,
-				Dropper:  dropper,
 			}
 
 			return agent.ApplyDisruption(cmd.Context(), disruptor, duration)
 		},
 	}
-
 	cmd.Flags().DurationVarP(&duration, "duration", "d", 0, "duration of the disruptions")
 	cmd.Flags().UintVarP(&filter.Port, "port", "p", 0, "target port of the connections to be disrupted")
-	cmd.Flags().Float64VarP(&dropRate, "rate", "r", 0, "fraction of connections to drop")
+	cmd.Flags().StringVarP(&filter.Protocol, "protocol", "P", "tcp", "target protocol of the connections to be disrupted")
 
 	return cmd
 }
